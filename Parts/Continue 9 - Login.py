80c77030cff02c7553badedf9c096ffc2b1e5904
Tuong Bao Nguyen <tuongnguyen48205@gmail.com>
2024-03-01 22:38:09 +1100

    Add files via upload

---
+++ Parts/Continue 9 - Login.py
@@ -0,0 +1,17 @@
+def continue9():
+    '''
+    Here we will clear up the screen
+    '''
+    global lbl_avatar
+    global lbl_pfp
+    global btn_upload
+    global frm_avatar
+    global frm_avatar_grid
+
+    lbl_avatar.forget()
+    lbl_pfp.forget()
+    btn_upload.forget()
+    frm_avatar.forget()
+    frm_avatar_grid.forget()
+
+    phase10()
